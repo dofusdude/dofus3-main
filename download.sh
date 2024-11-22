de17ddc36b85efb18b342479d85390dc69689c51
stelzo <stelzo@steado.de>
2024-11-22 22:41:46 +0100

    fix image res change

--- download.sh
+++ download.sh
@@ -14,7 +14,9 @@ else
     exit 1
 fi
 
-rm -rf out
+version=$1
+
+rm -rf out data
 
 curl -s https://api.github.com/repos/dofusdude/doduda/releases/latest \
     | grep "browser_download_url.*${os}_x86_64.tar.gz" \
@@ -26,7 +28,7 @@ tar -xzf "doduda_${os}_x86_64.tar.gz"
 rm "doduda_${os}_x86_64.tar.gz"
 chmod +x doduda
 
-./doduda --headless --release beta --output data
+./doduda --headless --release beta --version $version --output data
 echo "Done with loading"
 
 #./doduda map --headless --indent --release beta
@@ -34,19 +36,17 @@ echo "Done with loading"
 
 mkdir out
 
-ls -al
-
 mv data/*.json out/
 mv data/languages/*.json out/
-echo "Moved json files"
+echo "Found json data files"
 
-tar -czf items_images.tar.gz $(find data/img/item -name "*.png" ! -name "*-200.png")
+tar -czf items_images_64.tar.gz $(find data/img/item/1x -name "*.png")
 echo "Compressed all icon images"
-mv items_images.tar.gz out/
+mv items_images_64.tar.gz out/
 
-tar -czf items_images_200.tar.gz $(find data/img/item -name "*-200.png")
+tar -czf items_images_128.tar.gz $(find data/img/item/2x -name "*.png")
 echo "Compressed all sd images"
-mv items_images_200.tar.gz out/
+mv items_images_128.tar.gz out/
 
 echo "Cleaning up"
 rm -rf data
